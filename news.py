b37e569346b7e3bd59ccd3274f2d53eab29c0f68
flebster <39323299+flebster@users.noreply.github.com>
2026-07-13 12:29:48 -0400

    Update news.py

--- news.py
+++ news.py
@@ -1,1 +1,22 @@
-test
+#--------
+#PersonalDashboard
+#Module 1 - RSS Reader
+#--------
+
+from pathlib import Path
+PROJECT_FOLDER=Path(_file_).parent
+FEEDS_FILE=PROJECT_FOLDER / "feeds.txt"
+feeds = []
+with FEEDS_FILE.open("r", encoding="utf-8") as file:
+    for line in file:
+            line = line.strip()
+            
+            if line == "":
+                continue
+                
+                feeds.append(line)
+                
+print("Feeds found:")
+
+for feed in feeds:
+    print(" '", feed)
