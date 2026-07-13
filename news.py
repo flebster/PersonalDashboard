52f146066458cc9305ff438944adc80f9f117d58
flebster <39323299+flebster@users.noreply.github.com>
2026-07-13 18:09:08 -0400

    Update news.py

--- news.py
+++ news.py
@@ -6,11 +6,14 @@
 from pathlib import Path
 PROJECT_FOLDER=Path(__file__).parent
 FEEDS_FILE=PROJECT_FOLDER/"feeds.txt"
+print("Project folder:", PROJECT_FOLDER)
+print("Feeds File:", FEEDS_FILE)
+print("Exists?:", FEEDS_FILE.exists())
 feeds = []
 with FEEDS_FILE.open("r", encoding="utf-8") as file:
     for line in file:
             line = line.strip()
-            
+            print("Line Read:",repr(linea))
             if line == "":
                 continue
                 parts=line.split("|")
@@ -19,6 +22,7 @@ with FEEDS_FILE.open("r", encoding="utf-8") as file:
                     "url": parts[1],
                     "category": parts[2]
                 }
+               print("Adding:",feed)
                 feeds.append(feed)
                 
 print("Feeds loaded:")
