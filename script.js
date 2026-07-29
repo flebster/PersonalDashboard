d52054f3c3064e40a846274bb24175d18d0aec4f
flebster <39323299+flebster@users.noreply.github.com>
2026-07-28 22:23:31 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+fetch("news.json")
+
+.then(response => response.json())
+
+.then(data => {
+
+
+let container =
+document.getElementById("articles");
+
+
+container.innerHTML="";
+
+
+data.articles.forEach(article => {
+
+
+let card =
+document.createElement("div");
+
+
+card.className="card";
+
+
+card.innerHTML = `
+
+<h2>${article.title}</h2>
+
+<p class="source">
+${article.source || ""}
+</p>
+
+<button>
+Read Article
+</button>
+
+`;
+
+
+container.appendChild(card);
+
+
+});
+
+
+});
